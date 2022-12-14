337732cb8ee2cbefe5d0407df8634e8524ddfb87
Jan <14363947+jmne@users.noreply.github.com>
2022-12-14 22:33:56 +0100

    Implemented reading of the images

--- src/Main.py
+++ src/Main.py
@@ -1,16 +1,18 @@
-# Authors: Devin, Julian, Jan
-# Date: 2020-12-13
-# Description: Main file for the project
+from matplotlib import pyplot as plt
 from src.utils import ReadFile
+import time
 
 
 def main():
-    """
-    Prints the current state of the system.
+    start = time.time()
+    test_images, train_images, valid_images = ReadFile.read_images(read_from_processed=True) # you need to process them once before!
+    print("Train images: ", len(train_images))
+    print("Valid images: ", len(valid_images))
+    print("Test images: ", len(test_images))
+    plt.imshow(train_images[6][:, :, :])
+    plt.show()
 
-    Args:
-    """
-    ReadFile.read_images()
+    print("Compute Time: ", round(time.time() - start), "s")
 
 
 if __name__ == '__main__':
--- src/utils/ReadFile.py
+++ src/utils/ReadFile.py
@@ -4,23 +4,36 @@ import matplotlib.image as mpimg
 import numpy as np
 
 
-def read_images():
-    """Read folder with images
-
-    Returns:
-        array: array with folder names
-    """
-
+def read_images(read_from_processed=False):
     test_images = []
+    train_images = []
+    valid_images = []
+    invalid = 0
 
-    # get the path/directory
-    folder_dir = "../resources/Test"
-    for folder in listdir(folder_dir):
-        print(folder)
-        for file in listdir(folder_dir + "/" + folder):
-            test_images.append(mpimg.imread(folder_dir + "/" + folder + "/" + file))
-    print(test_images)
-    return test_images
+    if read_from_processed:
+        test_images = np.load("../resources/processed/test_images.npy", allow_pickle=True)
+        train_images = np.load("../resources/processed/train_images.npy", allow_pickle=True)
+        valid_images = np.load("../resources/processed/valid_images.npy", allow_pickle=True)
+    else:
+        folder_dir = "../resources"
+        for _dir in os.listdir(folder_dir):
+            if _dir == "processed":
+                continue
+            for folder in listdir(folder_dir + "/" + _dir):
+                for file in listdir(folder_dir + "/" + _dir + "/" + folder):
+                    if (folder_dir + "/" + _dir + "/" + folder + "/" + file).endswith(".jpeg"):
+                        if _dir == "test":
+                            test_images.append(mpimg.imread(folder_dir + "/" + _dir + "/" + folder + "/" + file))
+                        if _dir == "train":
+                            train_images.append(mpimg.imread(folder_dir + "/" + _dir + "/" + folder + "/" + file))
+                        if _dir == "valid":
+                            valid_images.append(mpimg.imread(folder_dir + "/" + _dir + "/" + folder + "/" + file))
+                    else:
+                        invalid += 1
+        print("Invalid images: ", invalid)
 
+        np.save("../resources/processed/test_images.npy", np.asarray(test_images, dtype=object))
+        np.save("../resources/processed/train_images.npy", np.asarray(train_images, dtype=object))
+        np.save("../resources/processed/valid_images.npy", np.asarray(valid_images, dtype=object))
 
-        
+    return test_images, train_images, valid_images
