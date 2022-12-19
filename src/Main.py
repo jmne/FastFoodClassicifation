babd2776413b51238a59b73070e66193a670103a
devinprehn <dcprehn37@gmail.com>
2022-12-19 17:55:15 +0100

    Updated zca() filtering to include batch size, for progress updates

--- src/Main.py
+++ src/Main.py
@@ -34,24 +34,32 @@ def resize(images, width, height):
 
     return new_images
 
-def zca(images):
+def zca(images, batch_size=100):
     # Step 1
-    images = images.reshape((images.shape[0], width*height*rgb))
-    # Step 2
-    images_norm = images / 255
-    # Step 3
-    images_norm = images_norm - images_norm.mean(axis=0)
-    # Step 4
-    cov = np.cov(images_norm, rowvar=False)
-    # Step 5
-    U,S,V = np.linalg.svd(cov)
-    # Step 6
-    epsilon = 0.1
-    # images_zca = U.dot(np.diag(1.0/np.sqrt(np.diag(S) + epsilon))).dot(U.T).dot(images_norm.T).T # Mine
-    images_zca = U.dot(np.diag(1.0/np.sqrt(S + epsilon))).dot(U.T).dot(images_norm.T).T # Tutorial
-    # And rescale
-    images_zca_rescaled = (images_zca - images_zca.min()) / (images_zca.max() - images_zca.min())
-    return images_zca_rescaled;
+    images = images.reshape((images.shape[0], images.shape[1]*images.shape[2]*images.shape[3]))
+    zca_images = np.empty(images.shape)
+    num_batches = math.ceil(images.shape[0] / batch_size)
+    for i in range(num_batches):
+        start_idx = batch_size * i
+        end_idx = min(batch_size * (i+1), images.shape[0])
+        batch = images[start_idx : end_idx]
+        print("Filtering images", start_idx + 1, "through", end_idx)
+        # Step 2
+        batch_norm = batch / 255
+        # Step 3
+        batch_norm = batch_norm - batch_norm.mean(axis=0)
+        # Step 4
+        cov = np.cov(batch_norm, rowvar=False)
+        # Step 5
+        U,S,V = np.linalg.svd(cov)
+        # Step 6
+        epsilon = 0.1
+        # images_zca = U.dot(np.diag(1.0/np.sqrt(np.diag(S) + epsilon))).dot(U.T).dot(images_norm.T).T # Mine
+        batch_zca = U.dot(np.diag(1.0/np.sqrt(S + epsilon))).dot(U.T).dot(batch_norm.T).T # Tutorial
+        # And rescale
+        batch_zca_rescaled = (batch_zca - batch_zca.min()) / (batch_zca.max() - batch_zca.min())
+        zca_images[start_idx : end_idx] = batch_zca_rescaled
+    return zca_images;
 
 
 def main():
@@ -78,14 +86,28 @@ def main():
     print("Time elapsed: ", time.time() - start)
     
     # Preprocessing
-    IM_WIDTH, IM_HEIGHT = (64, 64)
+    IM_WIDTH, IM_HEIGHT = (32, 32)
     print("Converting images to size {} x {}...".format(IM_WIDTH, IM_HEIGHT))
 
     start = time.time()
     train_images = resize(train_images, IM_WIDTH, IM_HEIGHT)
+    plt.figure(figsize=(10, 10))
     for i in range(9):
         ax = plt.subplot(3, 3, i + 1)
         plt.imshow(train_images[i])
+        plt.axis("off")
+        plt.title(y_train[i])
+    plt.show()
+    print("Time elapsed: ", time.time() - start)
+
+    print("Performing ZCA on images...")
+
+    start = time.time()
+    train_images = zca(train_images[:100])
+    plt.figure(figsize=(10, 10))
+    for i in range(9):
+        ax = plt.subplot(3, 3, i + 1)
+        plt.imshow(train_images[i].reshape((IM_WIDTH, IM_HEIGHT, 3)))
         plt.axis("off")
         plt.title(y_train[i])
     plt.show()
