9d33106204512522e36bece1a2a813fda2417e36
Jan <14363947+jmne@users.noreply.github.com>
2022-12-23 03:56:31 +0100

    Insert log and update Model3.py

--- src/Main.py
+++ src/Main.py
@@ -1,9 +1,11 @@
+import os
+
 from matplotlib import pyplot as plt
-from models import Model1, Model2
-from src.models import Model3
+from models import Model1, Model2, Model3
 from utils import ReadFile
 import time
 from random import randrange
+import sys
 
 
 def main():
@@ -12,30 +14,42 @@ def main():
 
     Args:
     """
+    print("Start model..")
+    if not os.path.isdir('../logs'):
+        os.mkdir('../logs')
+    if not os.path.isdir('../logs/plots'):
+        os.mkdir('../logs/plots')
+    open('../logs/log.txt', 'w').close()
+    sys.stdout = open('../logs/log.txt', 'a')
     start = time.time()
-    print("Start reading images")
-    test_images, train_images, valid_images, y_test, y_train, y_valid = ReadFile.read_images(
-        read_from_processed=True)  # you need to process them once before!
-    print("Train images: ", len(train_images))
-    print("Valid images: ", len(valid_images))
-    print("Test images: ", len(test_images))
-    plt.figure(figsize=(10, 10))
-    for i in range(9):
-        ax = plt.subplot(3, 3, i + 1)
-        rand = randrange(len(train_images)-1)
-        plt.imshow(train_images[rand])
-        plt.axis("off")
-        plt.title(y_train[rand])
-    plt.show()
-
-    print("Time elapsed: ", round(time.time() - start), "s")
-    print("Starting model...")
+    # test_images, train_images, valid_images, y_test, y_train, y_valid = ReadFile.read_images(
+    #     read_from_processed=False)  # you need to process them once before!
+    # print("Train images: ", len(train_images))
+    # print("Valid images: ", len(valid_images))
+    # print("Test images: ", len(test_images))
+    # plt.figure(figsize=(10, 10))
+    # for i in range(9):
+    #     ax = plt.subplot(3, 3, i + 1)
+    #     rand = randrange(len(train_images)-1)
+    #     plt.imshow(train_images[rand])
+    #     plt.axis("off")
+    #     plt.title(y_train[rand])
+    # plt.show()
+    #
+    print("Time elapsed: ", round(time.time() - start), "s", file=sys.stderr)
+    print("Starting model...", file=sys.stderr)
 
     # Model1.model()
     # Model2.model()
     Model3.model()
 
-    print("Compute Time: ", round(time.time() - start), "s")
+    print("Compute Time: ", round(time.time() - start), "s", file=sys.stderr)
+
+    sys.stdout.close()
+
+    print("Done!", file=sys.stderr)
+
+    exit(0)
 
 
 if __name__ == '__main__':
--- src/models/Model3.py
+++ src/models/Model3.py
@@ -1,4 +1,5 @@
 # Common
+from datetime import datetime
 import os
 import numpy as np
 import pandas as pd
@@ -6,7 +7,7 @@ import tensorflow as tf
 from IPython.display import clear_output as cls
 
 # Data Loading
-from keras.preprocessing.image import ImageDataGenerator as IDG
+from keras.preprocessing.image import ImageDataGenerator as Idg
 
 # Data Visualization
 import plotly.express as px
@@ -14,22 +15,27 @@ import matplotlib.pyplot as plt
 
 # Model
 from keras.models import Sequential
-from tensorflow_hub import KerasLayer as KL
+from plotly.io import write_image
+from tensorflow_hub import KerasLayer as Kl
 from keras.layers import Dense, InputLayer, Dropout
 
 # Optimizer
 from keras.optimizers import SGD
 from tensorflow import keras
-from keras import optimizers
-from keras.optimizers import schedules
 
 # Callbacks
-from keras.callbacks import EarlyStopping as ES, ModelCheckpoint as MC
+from keras.callbacks import EarlyStopping as Es, ModelCheckpoint as Mc
+
+print("\n")
+
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '1'
 
 
-def show_images(data, class_names, model=None, GRID=[5, 6], SIZE=(25, 25)):
+def show_images(data, classes, plot_num, _model=None, grid=None, SIZE=(25, 25)):
     # Plot Configurations
-    n_rows, n_cols = GRID
+    if grid is None:
+        grid = [5, 6]
+    n_rows, n_cols = grid
     n_images = n_cols * n_rows
     plt.figure(figsize=SIZE)
 
@@ -38,18 +44,18 @@ def show_images(data, class_names, model=None, GRID=[5, 6], SIZE=(25, 25)):
     for images, labels in iter(data):
 
         # Select data Randomly
-        id = np.random.randint(len(images))
-        image, label = tf.expand_dims(images[id], axis=0), class_names[int(labels[id])]
+        _id = np.random.randint(len(images))
+        image, label = tf.expand_dims(images[_id], axis=0), classes[int(labels[_id])]
 
         # Show Image
         plt.subplot(n_rows, n_cols, i)
         plt.imshow(image[0])
 
         # make Prediction
-        if model is not None:
-            prediction = model.predict(image)[0]
+        if _model is not None:
+            prediction = _model.predict(image)[0]
             score = np.round(max(prediction), 2)
-            pred = class_names[np.argmax(prediction)]
+            pred = classes[np.argmax(prediction)]
             title = f"True : {label}\nPred : {pred}"
         else:
             title = label
@@ -64,91 +70,101 @@ def show_images(data, class_names, model=None, GRID=[5, 6], SIZE=(25, 25)):
             break
 
     # Show Final Plot
+    fig = plt
+    fig.savefig(fname='../logs/plots/plot_' + str(plot_num) + '.png', format='png')
     plt.show()
 
 
-# Specify the root path.
-train_path = '../resources/train/'
-valid_path = '../resources/valid/'
+def model():
+    # Specify the root path.
+    train_path = '../resources/train/'
+    valid_path = '../resources/valid/'
 
-# Collect the class names.
-class_names = sorted(os.listdir(train_path))
-n_classes = len(class_names)
+    # Collect the class names.
+    class_names = sorted(os.listdir(train_path))
+    n_classes = len(class_names)
 
-# Print
-print("No. Classes : {}".format(n_classes))
-print("Classes     : {}".format(class_names))
+    # Print
+    print("No. Classes : {}".format(n_classes))
+    print("Classes     : {}".format(class_names))
 
-# Calculate the class distribution
-class_dis = [len(os.listdir(valid_path + name)) for name in class_names]
+    # Calculate the class distribution
+    class_dis = [len(os.listdir(valid_path + name)) for name in class_names]
 
-# Virtualize class distribution
-fig = px.pie(names=class_names, values=class_dis, hole=0.3)
-fig.update_layout({"title": {'text': "Validation Class Distribution", 'x': 0.48}})
-fig.show()
+    # Virtualize class distribution
+    fig1 = px.pie(names=class_names, values=class_dis, hole=0.3)
+    fig1.update_layout({"title": {'text': "Validation Class Distribution", 'x': 0.48}})
+    fig1.show()
+    write_image(fig=fig1, file='../logs/plots/plot_1.png', format='png')
 
-fig = px.bar(x=class_names, y=class_dis, color=class_names)
-fig.show()
+    fig2 = px.bar(x=class_names, y=class_dis, color=class_names)
+    fig2.show()
+    write_image(fig=fig2, file='../logs/plots/plot_2.png', format='png')
 
-# Initialize image data generator
-train_gen = IDG(rescale=1. / 255, rotation_range=10, horizontal_flip=True, vertical_flip=False)
-valid_gen = IDG(rescale=1. / 255)
+    # Initialize image data generator
+    train_gen = Idg(rescale=1. / 255, rotation_range=10, horizontal_flip=True, vertical_flip=False)
+    valid_gen = Idg(rescale=1. / 255)
 
-# Load the datasets
-train_ds = train_gen.flow_from_directory(train_path, shuffle=True, batch_size=64, target_size=(256, 256),
-                                         class_mode='binary')
-valid_ds = valid_gen.flow_from_directory(valid_path, shuffle=True, batch_size=32, target_size=(256, 256),
-                                         class_mode='binary')
+    # Load the datasets
+    train_ds = train_gen.flow_from_directory(train_path, shuffle=True, batch_size=64, target_size=(256, 256),
+                                             class_mode='binary')
+    valid_ds = valid_gen.flow_from_directory(valid_path, shuffle=True, batch_size=32, target_size=(256, 256),
+                                             class_mode='binary')
 
-show_images(data=train_ds, class_names=class_names)
+    show_images(data=train_ds, classes=class_names, plot_num=3)
 
-show_images(data=valid_ds, class_names=class_names)
+    show_images(data=valid_ds, classes=class_names, plot_num=4)
 
-# Model URL
-url = "https://tfhub.dev/google/bit/m-r50x1/1"
+    # Model URL
+    url = "https://tfhub.dev/google/bit/m-r50x1/1"
 
-# Load Model
-bit = KL(url)
+    # Load Model
+    bit = Kl(url)
 
-# Model Name
-model_name = "Fast-Food-Classification-BiT"
+    # Model Name
+    model_name = "Fast-Food-Classification-BiT"
 
-# Model Architecture
-model = Sequential([
-    InputLayer(input_shape=(256, 256, 3)),
-    bit,
-    Dropout(0.2),
-    Dense(n_classes, activation='softmax', kernel_initializer='zeros')
-], name=model_name)
+    # Model Architecture
+    _model = Sequential([
+        InputLayer(input_shape=(256, 256, 3)),
+        bit,
+        Dropout(0.2),
+        Dense(n_classes, activation='softmax', kernel_initializer='zeros')
+    ], name=model_name)
 
-# Model Summary
-model.summary()
+    # Model Summary
+    _model.summary()
 
-# Learning Rate Scheduler
-lr = 5e-3
+    # Learning Rate Scheduler
+    lr = 5e-3
 
-lr_scheduler = keras.optimizers.schedules.PiecewiseConstantDecay(boundaries=[200, 300, 400], values=[lr * 0.1, lr * 0.01, lr * 0.001, lr * 0.0001])
+    lr_scheduler = keras.optimizers.schedules.PiecewiseConstantDecay(boundaries=[200, 300, 400],
+                                                                     values=[lr * 0.1, lr * 0.01, lr * 0.001,
+                                                                             lr * 0.0001])
 
-opt = SGD(learning_rate=lr_scheduler, momentum=0.9)
+    opt = SGD(learning_rate=lr_scheduler, momentum=0.9)
 
-# Compile
-model.compile(
-    loss='sparse_categorical_crossentropy',
-    optimizer=opt,
-    metrics=['accuracy']
-)
+    # Compile
+    _model.compile(
+        loss='sparse_categorical_crossentropy',
+        optimizer=opt,
+        metrics=['accuracy']
+    )
 
-# Callbacks
-cbs = [ES(patience=5, restore_best_weights=True), MC(model_name + ".h5", save_best_only=True)]
+    # Callbacks
+    cbs = [Es(patience=5, restore_best_weights=True), Mc(model_name + ".h5", save_best_only=True)]
 
-# Training
-history = model.fit(train_ds, validation_data=valid_ds, epochs=50, callbacks=cbs)
+    # Training
+    history = _model.fit(train_ds, validation_data=valid_ds, epochs=50, callbacks=cbs)
 
-lc = pd.DataFrame(history.history)
-print(lc)
+    lc = pd.DataFrame(history.history)
+    print(lc)
 
-lc.plot(figsize=(8, 5))
-plt.title("Learning Curve")
-plt.grid()
+    lc.plot(figsize=(8, 5))
+    plt.title("Learning Curve")
+    plt.grid()
+    fig = plt
+    fig.savefig(fname='../logs/plots/plot_5.png', format='png')
+    plt.show()
 
-show_images(data=valid_ds, class_names=class_names, model=model)
+    show_images(data=valid_ds, classes=class_names, _model=_model, plot_num=6)
